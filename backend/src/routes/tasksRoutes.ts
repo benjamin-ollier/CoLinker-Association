fc5e2ca7944fe1f333d6a3d65ef2cf961adc311d
Louis <louis.driffort@gmail.com>
2024-05-20 22:55:08 +0200

    [tasks] add delete route by id for task

--- backend/src/routes/tasksRoutes.ts
+++ backend/src/routes/tasksRoutes.ts
@@ -15,6 +15,16 @@ router.get('/', async (req: Request, res: Response, next: NextFunction) => {
   }
 });
 
+router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const allTasks = await Task.find();
+    if (allTasks.length == 0) return res.json({message: "Il n'y a aucune tâche"}).sendStatus(200);
+    return res.json(allTasks).sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { username, dateDebut, dateFin, title } = req.body;
@@ -37,5 +47,25 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
     next(error);
   }
 })
+
+
+
+router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params;
+    if (!Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID invalide" });
+    }
+
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json({ message: "Tâche non trouvée" });
+    }
+
+    return res.status(200).json({ message: 'Tâche supprimée avec succès' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
